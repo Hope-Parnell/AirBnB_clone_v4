e7dd76977ab8a47835585c00c8e22d4a2d3f3473
Hope-Parnell <parnell.hope.c@gmail.com>
2022-01-13 11:48:18 -0600

    finshed task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,15 @@
 $(document).ready(() => {
-    
-}
+  const amenitiesDict = {};
+  $('li input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenitiesDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenitiesDict[$(this).data('id')];
+    }
+    const values = Object.values(amenitiesDict);
+    let str = values.join(', ');
+    if (str.length > 30) { str = str.slice(0, 30) + '...'; }
+    $('.amenities h4').text(str);
+    if (values.length === 0) $('.amenities h4').html('&nbsp;');
+  });
+});
