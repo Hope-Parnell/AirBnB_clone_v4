99f87d5298fa7e57f78652fa739bc50274d8a89f
Hope-Parnell <parnell.hope.c@gmail.com>
2022-01-13 14:58:09 -0600

    task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,54 @@
+$(document).ready(() => {
+  placesPost('{}');
+  const amenitiesDict = {};
+  $('li input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenitiesDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenitiesDict[$(this).data('id')];
+    }
+    const values = Object.values(amenitiesDict);
+    let str = values.join(', ');
+    if (str.length > 30) { str = str.slice(0, 30) + '...'; }
+    $('.amenities h4').text(str);
+    if (values.length === 0) $('.amenities h4').html('&nbsp;');
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+  $('button').click((evt) => {
+    evt.preventDefault();
+    $('article').remove();
+    placesPost(JSON.stringify({ amenities: Object.keys(amenitiesDict) }));
+  });
+  function placesPost (input) {
+    $.post('http://0.0.0.0:5001/api/v1/places_search/', input, (places) => {
+      for (const place of places) {
+        const guestS = (place.max_guest !== 1) ? 's' : '';
+        const roomS = (place.number_rooms !== 1) ? 's' : '';
+        const bathroomS = (place.number_bathrooms !== 1) ? 's' : '';
+        const html = `<article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest${guestS}</div>
+                    <div class="number_rooms">${place.number_rooms} Bedroom${roomS}</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathroom${bathroomS}</div>
+            </div>
+            <div class="user">
+                  </div>
+                  <div class="description">
+              ${place.description}
+                  </div>
+          </article>`;
+        $('.places').append(html);
+      }
+    }, 'json');
+  }
+});
