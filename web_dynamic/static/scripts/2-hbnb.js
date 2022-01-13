d79935d462cbb274923fde4601501d916b483889
mutantEris <mmires@live.com>
2022-01-13 12:55:06 -0600

    Task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+  const amenitiesDict = {};
+  $('li input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenitiesDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenitiesDict[$(this).data('id')];
+    }
+    const values = Object.values(amenitiesDict);
+    let str = values.join(', ');
+    if (str.length > 30) { str = str.slice(0, 30) + '...'; }
+    $('.amenities h4').text(str);
+    if (values.length === 0) $('.amenities h4').html('&nbsp;');
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
